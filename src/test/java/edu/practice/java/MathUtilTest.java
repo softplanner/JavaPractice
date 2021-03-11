e69cd22c79b2cc2d27574aa9660d54a289cd1d27
Muhammad Jawad <softplanner@gmail.com>
2021-03-11 20:33:59 +0100

    Test life-cycle hook annotation using tag "@BeforeEach".

--- src/test/java/edu/practice/java/MathUtilTest.java
+++ src/test/java/edu/practice/java/MathUtilTest.java
@@ -1,14 +1,22 @@
 package edu.practice.java;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.lang.Exception.*;
 
 public class MathUtilTest {
 
+    MathUtil mathUtils;
+
+    @BeforeEach
+    public void init(){
+        mathUtils = new MathUtil();
+    }
+
     @Test
     public void testAdd(){
-        MathUtil mathUtils = new MathUtil();
         int expected = 2;
         int actual = mathUtils.add(1,1);
         assertEquals(expected, actual);
@@ -16,7 +24,6 @@ public class MathUtilTest {
 
     @Test
     public void testCircleArea(){
-        MathUtil mathUtils = new MathUtil();
         assertEquals(314.1592653589793,
                 mathUtils.computeCircleArea(10.0),
                 "should return correct area value");
@@ -24,7 +31,6 @@ public class MathUtilTest {
 
     @Test
     public void testDivide(){
-        MathUtil mathUtils = new MathUtil();
         assertThrows(ArithmeticException.class,
                 () -> mathUtils.divide(1,0),
                 "divide by 0 should throw exception.");
