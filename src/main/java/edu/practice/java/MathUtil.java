685b053d03462e78fd9f67ba35086ec8e0cf755c
Muhammad Jawad <softplanner@gmail.com>
2021-03-11 20:23:33 +0100

    Exception throws example with lambda expression on divide method.

--- src/main/java/edu/practice/java/MathUtil.java
+++ src/main/java/edu/practice/java/MathUtil.java
@@ -9,4 +9,8 @@ public class MathUtil {
     public double computeCircleArea(double radius){
         return Math.PI * radius * radius;
     }
+
+    public int divide(int numerator, int denominator){
+        return numerator/denominator;
+    }
 }
--- src/test/java/edu/practice/java/MathUtilTest.java
+++ src/test/java/edu/practice/java/MathUtilTest.java
@@ -2,11 +2,12 @@ package edu.practice.java;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.lang.Exception.*;
 
 public class MathUtilTest {
 
     @Test
-    public void addTest(){
+    public void testAdd(){
         MathUtil mathUtils = new MathUtil();
         int expected = 2;
         int actual = mathUtils.add(1,1);
@@ -14,10 +15,18 @@ public class MathUtilTest {
     }
 
     @Test
-    public void circleAreaTest(){
+    public void testCircleArea(){
         MathUtil mathUtils = new MathUtil();
         assertEquals(314.1592653589793,
                 mathUtils.computeCircleArea(10.0),
                 "should return correct area value");
+    }
+
+    @Test
+    public void testDivide(){
+        MathUtil mathUtils = new MathUtil();
+        assertThrows(ArithmeticException.class,
+                () -> mathUtils.divide(1,0),
+                "divide by 0 should throw exception.");
     }
 }
